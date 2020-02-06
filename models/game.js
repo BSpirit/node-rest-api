8eba810119c48c730d156c6c28d7e85953e1e742
Tony Clonier <clonier.tony@hotmail.fr>
2020-02-06 14:50:20 +0100

    Added Game mongoose schema

---
+++ models/game.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+
+const gameSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 350,
+    },
+    editor: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 250,
+    },
+    categories: {
+        type: [String],
+        required: true,
+        enum: ['adventure', 'action', 'multiplayer', 'race', 'role games', 'simulation', 'shooting', 'sports', 'strategy'],
+        validate: {
+            validator: function(array) {
+                return array && array.length >= 1;
+            }
+        }
+    },
+    stock: {
+        type: Number,
+        default: 0,
+        min: 0,
+    },
+    releaseDate: {
+        type: Date,
+        default: Date.now(),
+    },
+    isAwardWinner: {
+        type: Boolean,
+        default: false,
+    }
+});
+
+module.exports = mongoose.model('Game', gameSchema);
---
+++ models/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+
+mongoose
+    .connect('mongodb://localhost:27017/socksdb', { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(`Could not connect to MongoDB: ${err}`));
+
+
+module.exports.Game = require('./game.js');
